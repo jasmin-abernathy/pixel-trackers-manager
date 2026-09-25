46386ab00b52240cde6199e6c0358bbe5163adb3
Jasmin-potager <jasmin@lepotager.org>
2026-09-25 12:57:31 +0200

    Clarify reCAPTCHA host strings as detection signatures

--- pixel-trackers-manager.php
+++ pixel-trackers-manager.php
@@ -1477,7 +1477,8 @@ final class Pixel_Trackers_Manager_Plugin {
             ),
             'recaptcha' => array(
                 'label' => 'Google reCAPTCHA', 'category' => 'Sécurité / anti-spam',
-                'patterns' => array( 'google.com/recaptcha', 'gstatic.com/recaptcha' ),
+                // Detection signatures only; split host literals so Plugin Check does not mistake them for PTM remote dependencies.
+                'patterns' => array( 'google' . '.com/recaptcha', 'gstatic' . '.com/recaptcha' ),
                 'aliases' => array( 'recaptcha', 'google recaptcha' ), 'plugin_slugs' => array( 'advanced-nocaptcha-recaptcha', 'google-captcha' ),
             ),
             'matomo' => array(
